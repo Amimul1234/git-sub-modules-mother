5873f127f798afe696b312f91f3dddbcef2a2254
amimul.inument <amimul.inument@bkash.com>
2025-03-12 12:36:30 +0600

    test git sub modules

---
+++ src/main/java/com/rahi/submodues/SubModuesApplication.java
@@ -0,0 +1,13 @@
+package com.rahi.submodues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubModuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubModuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rahi/submodues/SubModuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahi.submodues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubModuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
